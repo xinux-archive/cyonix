1d6ae5e8b480287bd9432ea9d8f619750d543715
phoenixifier <phoenixbboy@gmail.com>
2023-02-21 13:05:07 +0500

    update .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+
+    let main = format!("../{}", ".cyonix");
+    let submain = format!("../{}", ".cyonix/storage");
+    fs::create_dir(main)?;
+    fs::create_dir(submain)?;
+    let mut file = File::create("../.cyonix/file_list")?;
+
+    Ok(())
 }
